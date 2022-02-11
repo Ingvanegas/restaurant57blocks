f16f4a6c362ff8a15c9ae00edb98a9639fa2fe9d
Walter vanegas <walter.vanegas@globant.com>
2022-02-11 10:56:43 -0500

    warning message

--- src/database/actions.ts
+++ src/database/actions.ts
@@ -1,5 +1,8 @@
 const sequalize = require('sequelize');
-const database = new sequalize('mysql://admin:restaurant@restaurant.c4iulrsbmpko.us-east-2.rds.amazonaws.com:3306/restaurant');
+
+//!WARNING This connection string is here just for evaluation purposes to production is necessary to move to a environment variable
+const database = new sequalize('mysql://admin:restaurant@restaurant.c4iulrsbmpko.us-east-2.rds.amazonaws.com:3306/restaurant'); 
+//!WARNING This connection string is here just for evaluation purposes to production is necessary to move to a environment variable
 
 export const get = async (sentence, parameters) => {
     return await database.query(sentence, 
